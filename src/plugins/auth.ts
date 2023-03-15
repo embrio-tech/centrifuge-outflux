5bbd954cd9249cd84c05e99da8bbbb17445fd5b7
Timon Heinis <timon@embrio.tech>
2023-03-15 12:09:14 +0100

    feat: require the definition of an API key

--- src/plugins/auth.ts
+++ src/plugins/auth.ts
@@ -5,6 +5,7 @@ import { API_KEY } from '../config'
 import { ApiError } from '../helpers'
 
 const plugin: FastifyPluginCallback = fp(async function (server) {
+  if (!API_KEY) throw new Error('There is no API_KEY specified on the environment!')
   server.decorate('verifyApiKey', async (request: FastifyRequest) => {
     const { authorization } = request.headers
     if (!authorization || authorization !== API_KEY) throw new ApiError(401, undefined, 'Please provide a valid API key!')
