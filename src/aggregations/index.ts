c35c655590de618413594ac9291d62a552377ec8
Timon Heinis <timon@embrio.tech>
2023-05-30 14:36:32 +0000

    refactor: remove unused aggregation pipelines

--- src/aggregations/index.ts
+++ src/aggregations/index.ts
@@ -1,3 +1,1 @@
-export { default as sumOfNormalizedDebtPerState } from './sumOfNormalizedDebtPerState'
-export { default as ficoWeightedByNormalizedDebt } from './ficoWeightedByNormalizedDebt'
 export { default as loansWithLatestFramePerSource } from './loansWithLatestFramePerSource'
